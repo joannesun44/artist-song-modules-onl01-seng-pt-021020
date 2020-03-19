0d3d60f25278cca424084e170eb8d3fdb6513a23
Joanne Qiong Sun <joannesun44@gmail.com>
2020-03-19 01:55:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,9 +21,13 @@ class Artist
     @songs = []
   end
   
-  def 
-
-
+  # def reset_all
+  #   self.all.clear
+  # end 
+  
+  # def count
+  #   self.all.count
+  # end 
 
   # def self.find_by_name(name)
   #   @@artists.detect{|a| a.name == name}
