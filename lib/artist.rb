86a06e6fcc1038c46232602c4d34e4031fb3b05a
Joanne Qiong Sun <joannesun44@gmail.com>
2020-03-19 01:01:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,6 +6,8 @@ require 'pry'
 class Artist
   
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
   
   attr_accessor :name
   attr_reader :songs
